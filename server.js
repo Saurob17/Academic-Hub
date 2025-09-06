42bcd83bf2995472f1430c65212b3be3a12fe3c0
MosabbirSAIF <saifmosabbir19196@gmail.com>
2025-09-06 13:03:05 +0600

    Home-page changed

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 //object
 const con = mysql.createConnection({
   host: '127.0.0.1',
-  user: 'root',
-  password: '1',
+  user: 'nodeuser',
+  password: 'NodePassword123',
   database: 'university'
 });
 
